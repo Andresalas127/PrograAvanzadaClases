d99129d78f30a4ab0876e800749a78702d774508
Andresalas127 <106887514+Andresalas127@users.noreply.github.com>
2023-10-23 14:27:41 -0600

    Cambios 3/10

---
+++ APIKNPrograAvanzada/APIKNPrograAvanzada/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using APIKNPrograAvanzada.Entities;
+using System.Web.Http;
+
+namespace APIKNPrograAvanzada.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("RegistrarCuenta")]
+        public string RegistrarCuenta(UsuarioEnt entidad)
+        {
+            using (var context = new BDKNEntities())
+            {
+                var user = new TUsuario();
+                user.Identificacion = entidad.Identificacion;
+                user.Nombre = entidad.Nombre;
+                user.Correo = entidad.Correo;
+                user.Contrasenna = entidad.Contrasenna;
+                user.Estado = entidad.Estado;
+                user.Direccion = entidad.Direccion;
+
+                context.TUsuario.Add(user);
+                context.SaveChanges();
+
+                return "Registro realizado correctamente";
+                                
+            }
+           
+           
+
+        }
+
+    }
+}
--- WebKNPrograAvanzada/WebKNPrograAvanzada/Models/UsuarioModel.cs
+++ WebKNPrograAvanzada/WebKNPrograAvanzada/Models/UsuarioModel.cs
@@ -11,15 +11,15 @@ namespace WebKNPrograAvanzada.Models
 
         public void IniciarSesion(UsuarioEnt entidad)
         {
-
+            //LLAMAR AL WEB API PARA VALIDAR EL ACCESO DEL USUARIO
         }
         public void RegistrarCuenta(UsuarioEnt entidad)
         {
-
+            //LLAMAR AL WEB API PARA REGISTRAR EL USUARIO
         }
         public void RecuperarCuenta(UsuarioEnt entidad)
         {
-
+            //LLAMAR AL WEB API PARA VALIDAR SI ES UN USUARIO VALIDO Y LE MANDO UN CORREO
         }
     }
 }
